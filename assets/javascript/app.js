766d6771ed95db63d1f913eebbe402484ede5b09
Mike Hayes <fikes428@gmail.com>
2019-08-05 14:14:28 -0400

    htmlLinks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,96 +14,100 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
-
     $(".submitInput").on("click", function (event) {
-        console.log("this works");
+		// console.log("this works");
 
-        var nameInput = $("#nameInput").val().trim();
-        var numberInput = $("#numberInput").val().trim();
-        var destinationInput = $("#destInput").val().trim();
-        var timeInput = $("#timeInput").val().trim();
-        var frequencyInput = $("#freqInput").val().trim();
+			var nameInput = $("#nameInput").val().trim();
 
+			var numberInput = $("#numberInput").val().trim();
 
-        if (nameInput != "" &&
-            numberInput != "" &&
-            destinationInput != "" &&
-            timeInput.length === 4 &&
-            frequencyInput != "") {
+			var destinationInput = $("#destInput").val().trim();
 
+			var timeInput = $("#timeInput").val().trim();
 
-            database.ref().push({
-                name: nameInput,
-                number: numberInput,
-                destination: destinationInput,
-                time: timeInput,
-                frequency: frequencyInput,
-            });
+			var frequencyInput = $("#freqInput").val().trim();
 
-        } else {
-            alert("Please enter valid train data");
-            $("input").val("");
-            return false;
-        }
-        //console.log(database);
+			//input validation
+			if (nameInput != "" &&
+				numberInput != "" &&
+				destinationInput != "" &&
+				timeInput.length === 4 &&
+				frequencyInput != "") {
 
-        $("input").val("");
+					//use the collected input (above) and port it to firebase db
+					database.ref().push({
+						name: nameInput,
+						number: numberInput,
+						destination: destinationInput,
+						time: timeInput,
+						frequency: frequencyInput,
+					});
 
-    });
+			} else {
+				alert("Please enter valid train data");
+				$("input").val("");
+				return false;
+			}
 
-    database.ref().on("child_added", function (childSnapshot) {
-        //console.log(childSnapshot.val());
+			//console.log(database);
 
-        var name = childSnapshot.val().name;
-        var number = childSnapshot.val().number;
-        var destination = childSnapshot.val().destination;
-        var time = childSnapshot.val().time;
-        var frequency = childSnapshot.val().frequency;
-            console.log(name, number, destination, time, frequency);
+			$("input").val("");
 
-        var frequency = parseInt(frequency);
-        var currentTime = moment();
+	});
 
-            console.log("Current Time: " + moment().format("HHmm"));
+	
+	database.ref().on("child_added", function (childSnapshot) {
+		// console.log(childSnapshot.val());
 
-        var dateConvert = moment(childSnapshot.val().time, "HHmm").subtract(1, "years");
-            console.log("DATE CONVERTED: " + dateConvert);
+		var name = childSnapshot.val().name;
+		var number = childSnapshot.val().number;
+		var destination = childSnapshot.val().destination;
+		var time = childSnapshot.val().time;
+		var frequency = childSnapshot.val().frequency;
+			console.log(name, number, destination, time, frequency);
 
-        var trainTime = moment(dateConvert).format("HHmm");
-            console.log("Train time : " + trainTime);
+		var frequency = parseInt(frequency);
+		var currentTime = moment();
+			console.log("Current time: " + moment().format("HHmm"));
 
-        var timeConvert = moment(trainTime, "HHmm").subtract(1, "years");
-        var timeDifference = moment().diff(moment(timeConvert), "minutes");
-            console.log("Difference in time: " + timeDifference);
+		var dateConvert = moment(childSnapshot.val().time, "HHmm").subtract(1, "years");
+			//console.log("DATE CONVERTED: " + dateConvert);
 
-        var timeRemaining = timeDifference % frequency;
-            console.log("Time remaining: " + timeRemaining);
+		var trainTime = moment(dateConvert).format("HHmm");
+			//console.log("Train time : " + trainTime);
 
-        var timeAway = frequency - timeRemaining;
+		var timeConvert = moment(trainTime, "HHmm").subtract(1, "years");
+		var timeDifference = moment().diff(moment(timeConvert), "minutes");
+			console.log("Difference in time: " + timeDifference);
 
-        var nextArrival = moment().add(timeAway, "minutes");
+		var timeRemaining = timeDifference % frequency;
+			//console.log("Time remaining: " + timeRemaining);
 
+		var timeAway = frequency - timeRemaining;
+			//console.log("Minutes until next train: " + timeAway);
+		
+		var nextArrival = moment().add(timeAway, "minutes");
+		//console.log("Arrival time: " + moment(nextArrival).format("HHmm"));
 
-        var arrivalDisplay = moment(nextArrival).format("HHmm");
+		var arrivalDisplay = moment(nextArrival).format("HHmm");
 
+	
+	$("#boardText").append(
+		"<tr><td id='nameDisplay'>" + childSnapshot.val().name + 
+		"<td id='numberDisplay'>" + childSnapshot.val().number + 
+		"<td id='destinationDisplay'>" + childSnapshot.val().destination + 
+		"<td id='frequencyDisplay'>" + childSnapshot.val().frequency +
+		"<td id='arrivalDisplay'>" + arrivalDisplay + 
+		"<td id='awayDisplay'>" + timeAway + " minutes until arrival" + "</td></tr>");
+		// console.log(arrivalDisplay);
+		// console.log(timeAway);
+	});
 
-        $("#boardText").append(
-            "<tr><td id='nameDisplay'>" + childSnapshot.val().name +
-            "<td id='numberDisplay'>" + childSnapshot.val().number +
-            "<td id='destinationDisplay'>" + childSnapshot.val().destination +
-            "<td id='frequencyDisplay'>" + childSnapshot.val().frequency +
-            "<td id='arrivalDisplay'>" + arrivalDisplay +
-            "<td id='awayDisplay'>" + timeAway + " minutes until arrival" + "</td></tr>");
-                
-                // console.log(arrivalDisplay);
-                // console.log(timeAway);
-    });
+	//reset functionality
+	$(".resetInput").on("click", function(event){
+    	location.reload();
+	});
 
-
-    $(".resetInput").on("click", function (event) {
-        location.reload();
-    });
-
-
-    setInterval("window.location.reload()", 60000);
+	
+	setInterval("window.location.reload()", 60000);
 });
