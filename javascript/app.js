ba1cb3faca5f46d1ad1e6c6cc1822b4fa97fa033
Mike Hayes <fikes428@gmail.com>
2019-08-05 11:32:13 -0400

    firebaseJSlink

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyC1zUvc5K6NJXFxwm74405gegKYfPSNT-4",
+    authDomain: "train-schedule-db.firebaseapp.com",
+    databaseURL: "https://train-schedule-db.firebaseio.com",
+    projectId: "train-schedule-db",
+    storageBucket: "train-schedule-db.appspot.com",
+    messagingSenderId: "60174437847"
+};
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
