d66a4c5741357d3d15b6e6388a45fd7398191ca3
Mike Hayes <fikes428@gmail.com>
2019-08-05 11:56:30 -0400

    firebaseLinkIssue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,12 @@
-var config = {
-    apiKey: "AIzaSyC1zUvc5K6NJXFxwm74405gegKYfPSNT-4",
-    authDomain: "train-schedule-db.firebaseapp.com",
-    databaseURL: "https://train-schedule-db.firebaseio.com",
-    projectId: "train-schedule-db",
-    storageBucket: "train-schedule-db.appspot.com",
-    messagingSenderId: "60174437847"
-};
-  
+// var config = {
+//     apiKey: "AIzaSyC1zUvc5K6NJXFxwm74405gegKYfPSNT-4",
+//     authDomain: "train-schedule-db.firebaseapp.com",
+//     databaseURL: "https://train-schedule-db.firebaseio.com",
+//     projectId: "train-schedule-db",
+//     storageBucket: "train-schedule-db.appspot.com",
+//     messagingSenderId: "60174437847"
+// };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
@@ -15,12 +15,27 @@ var database = firebase.database();
 $(".submitInput").on("click", function (event) {
     // console.log("this works");
 
-        var nameInput = $("#nameInput").val().trim();
+    var nameInput = $("#nameInput").val().trim();
 
-        var numberInput = $("#numberInput").val().trim();
+    var numberInput = $("#numberInput").val().trim();
 
-        var destinationInput = $("#destInput").val().trim();
+    var destinationInput = $("#destInput").val().trim();
 
-        var timeInput = $("#timeInput").val().trim();
+    var timeInput = $("#timeInput").val().trim();
 
-        var frequencyInput = $("#freqInput").val().trim();
+    var frequencyInput = $("#freqInput").val().trim();
+
+    if (nameInput != "" &&
+        numberInput != "" &&
+        destinationInput != "" &&
+        timeInput.length === 4 &&
+        frequencyInput != "") {
+
+        //send above data to firebase
+        database.ref().push({
+            name: nameInput,
+            number: numberInput,
+            destination: destinationInput,
+            time: timeInput,
+            frequency: frequencyInput,
+        });
